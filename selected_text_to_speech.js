e94e173f51667e22c1444afd99a8bb49fe946f34
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-08-20 10:04:19 -0400

    Update selected_text_to_speech.js

--- selected_text_to_speech.js
+++ selected_text_to_speech.js
@@ -1,16 +1,18 @@
+var formatDivContentAsString = (s) => s.replace(/<span>|<br>/g, '\n').replace(/<.+?>/g, '').trim();
 function getSelectionText() {
   var text = "";
   if (window.getSelection) text = window.getSelection().toString();
   if (document.selection && document.selection.type != "Control") text = document.selection.createRange().text;
-    return text;
+    return formatDivContentAsString(text);
 }
 function playSelection(){
   var selText = getSelectionText();
   var synth = window.speechSynthesis;
-  var utterThis = new SpeechSynthesisUtterance(selText ? selText : 'Nothing selected. Select some text, homie.');
-  utterThis.voice = synth.getVoices()[3];
+  var utterThis = new SpeechSynthesisUtterance(selText ? selText : 'Nothing selected.');
+  var voices = synth.getVoices();
+  utterThis.voice = voices[3];
   utterThis.pitch = '1';
-  utterThis.rate = '1.2';
+  utterThis.rate = '1.3';
   synth.speak(utterThis);
 }
 playSelection()
