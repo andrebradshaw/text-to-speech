8a9bad8f0f3b24de5d6bd7c6f6cfe35562d10c94
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-09-21 18:11:38 -0400

    Update tts_withActions.js

--- tts_withActions.js
+++ tts_withActions.js
@@ -7,20 +7,20 @@ var unq = (arr) => arr.filter((e, p, a) => a.indexOf(e) == p);
 var delay = (ms) => new Promise(res => setTimeout(res, ms));
 var ele = (t) => document.createElement(t);
 var attr = (o, k, v) => o.setAttribute(k, v);
-var reChar = (s) => typeof s == 'string' && s.match(/&#\d+;/g) && s.match(/&#\d+;/g).length > 0 ? s.match(/&#\d+;/g).map(el => [el, String.fromCharCode(reg(/(?<=&#).+?(?=;)/.exec(el),0))]).map(m => s = s.replace(new RegExp(m[0], 'i'), m[1])).pop() : s;
+var reChar = (s) => typeof s == 'string' && s.match(/&#\d+;/g) && s.match(/&#\d+;/g).length > 0 ? s.match(/&#\d+;/g).map(el => [el, String.fromCharCode(reg(/(?<=&#).+?(?=;)/.exec(el), 0))]).map(m => s = s.replace(new RegExp(m[0], 'i'), m[1])).pop() : s;
 var noHtmlEntities = (s) => typeof s == 'string' ? s.replace(/&amp;/g, '&').replace(/&quot;/g, '"').replace(/&apos;/g, "'").replace(/&nbsp;/g, ' ') : s;
 
 
 var formatDivContentAsString = (s) => s.replace(/<span>|<br>/g, '\n').replace(/<.+?>/g, '').trim();
 
-var svgs= {
-	close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(0.85, 0.85)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#e21212" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`,
-	play:`<svg version="1.1" x="0px" y="0px" viewBox="0 0 101 101" style="enable-background:new 0 0 100.25 100.25;" xml:space="preserve">
+var svgs = {
+  close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(0.85, 0.85)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#e21212" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`,
+  play: `<svg version="1.1" x="0px" y="0px" viewBox="0 0 101 101" style="enable-background:new 0 0 100.25 100.25;" xml:space="preserve">
 <g>	<path fill="#14b370" d="M69.817,48.243l-30-19.5c-0.461-0.3-1.05-0.322-1.533-0.061c-0.483,0.263-0.785,0.769-0.785,1.318v39   c0,0.55,0.301,1.056,0.785,1.318c0.224,0.121,0.47,0.182,0.715,0.182c0.285,0,0.57-0.081,0.817-0.242l30-19.5   c0.426-0.276,0.683-0.75,0.683-1.258S70.243,48.519,69.817,48.243z M40.5,66.237V32.764L66.248,49.5L40.5,66.237z"/>
 	<path fill="#14b370"  d="M49.5,6.5c-23.71,0-43,19.29-43,43s19.29,43,43,43s43-19.29,43-43S73.21,6.5,49.5,6.5z M49.5,89.5   c-22.056,0-40-17.944-40-40s17.944-40,40-40s40,17.944,40,40S71.556,89.5,49.5,89.5z"/>
 </g></svg>`,
-	pause: `<svg version="1.1" viewBox="0 0 129 129" enable-background="new 0 0 129 129">  <g>    <g>   <path fill="#eb4034" d="m64.5,122.6c32.1,0 58.1-26.1 58.1-58.1s-26-58.1-58.1-58.1-58.1,26-58.1,58.1 26,58.1 58.1,58.1zm0-108.1c27.5,0 50,22.4 50,50s-22.4,50-50,50-50-22.4-50-50 22.5-50 50-50z"/>      <path fill="#eb4034" d="m53.8,94.7c2.3,0 4.1-1.8 4.1-4.1v-53.1c0-2.3-1.8-4.1-4.1-4.1-2.3,0-4.1,1.8-4.1,4.1v53.1c7.10543e-15,2.3 1.8,4.1 4.1,4.1z"/>      <path fill="#eb4034" d="m75.2,94.7c2.3,0 4.1-1.8 4.1-4.1v-53.1c0-2.3-1.8-4.1-4.1-4.1-2.3,0-4.1,1.8-4.1,4.1v53.1c-1.42109e-14,2.3 1.8,4.1 4.1,4.1z"/>    </g>  </g></svg>`
-  };
+  pause: `<svg version="1.1" viewBox="0 0 129 129" enable-background="new 0 0 129 129">  <g>    <g>   <path fill="#eb4034" d="m64.5,122.6c32.1,0 58.1-26.1 58.1-58.1s-26-58.1-58.1-58.1-58.1,26-58.1,58.1 26,58.1 58.1,58.1zm0-108.1c27.5,0 50,22.4 50,50s-22.4,50-50,50-50-22.4-50-50 22.5-50 50-50z"/>      <path fill="#eb4034" d="m53.8,94.7c2.3,0 4.1-1.8 4.1-4.1v-53.1c0-2.3-1.8-4.1-4.1-4.1-2.3,0-4.1,1.8-4.1,4.1v53.1c7.10543e-15,2.3 1.8,4.1 4.1,4.1z"/>      <path fill="#eb4034" d="m75.2,94.7c2.3,0 4.1-1.8 4.1-4.1v-53.1c0-2.3-1.8-4.1-4.1-4.1-2.3,0-4.1,1.8-4.1,4.1v53.1c-1.42109e-14,2.3 1.8,4.1 4.1,4.1z"/>    </g>  </g></svg>`
+};
 
 
 function aninCloseBtn() {
@@ -80,42 +80,96 @@ function getSelectionText() {
   var text = "";
   if (window.getSelection) text = window.getSelection().toString();
   if (document.selection && document.selection.type != "Control") text = document.selection.createRange().text;
-    return noHtmlEntities(reChar(formatDivContentAsString(text)));
+  return noHtmlEntities(reChar(formatDivContentAsString(text)));
 }
 
 
-  function showLastWord(pos){
-    var spans = Array.from(cn(document,'wordStrmArr'));
-    var targs = spans.slice(0,(pos));
-    if(targs && targs.length > 0) {
-      targs.forEach(el=> {
-        el.style.background = '#08709c';
-        el.style.borderRadius = '0em';
-      });
-      var last = spans.slice((pos),(pos+11));
-      var i = last.map(el=> /\.|\s/.test(el.innerText)).indexOf(true);
-	  var chop = last.slice(0,i+1);
-      chop.forEach(el=> {
-        el.style.borderRadius = '0em';
-	    el.style.background = '#08709c';
-      });
-    if(chop[chop.length-1]){
-	  chop[chop.length-1].style.borderTopRightRadius = '0.9em';
-	  chop[chop.length-1].style.borderBottomRightRadius = '0.9em';
-	  chop[chop.length-1].style.background = '#08709c';
-    }
+function showLastWord(pos) {
+  var spans = Array.from(cn(document, 'wordStrmArr'));
+  var targs = spans.slice(0, (pos));
+  if (targs && targs.length > 0) {
+    targs.forEach(el => {
+      el.style.background = '#08709c';
+      el.style.borderRadius = '0em';
+    });
+    var last = spans.slice((pos), (pos + 11));
+    var i = last.map(el => /\.|\s/.test(el.innerText)).indexOf(true);
+    var chop = last.slice(0, i + 1);
+    chop.forEach(el => {
+      el.style.borderRadius = '0em';
+      el.style.background = '#08709c';
+    });
+    if (chop[chop.length - 1]) {
+      chop[chop.length - 1].style.borderTopRightRadius = '0.9em';
+      chop[chop.length - 1].style.borderBottomRightRadius = '0.9em';
+      chop[chop.length - 1].style.background = '#08709c';
     }
   }
+}
 
-async function playSelection(){
-var textDefault = 'This is a test of speaking like a human. Hopefully this will help you recognize that robots are humans too... You monster.';
+var opts = [
+  ["de-DE", "Deutsch"],
+  ["en-US", "English US"],
+  ["en-GB", "English UK"],
+  ["es-ES", "Español"],
+  ["es-US", "Español de Estados Unidos"],
+  ["fr-FR", "Français"],
+  ["hi-IN", "हिन्दी"],
+  ["id-ID", "Bahasa Indonesia"],
+  ["it-IT", "Italiano"],
+  ["ja-JP", "日本語"],
+  ["nl-NL", "Nederlands"],
+  ["pl-PL", "Polski"],
+  ["pt-BR", "Português do Brasil"],
+  ["ru-RU", "русский"],
+  ["ko-KR", "한국의"],
+  ["zh-CN", "普通话（中国大陆）"],
+  ["zh-HK", "粤語（香港）"],
+  ["zh-TW", "國語（臺灣）"]
+];
+
+function hoverSwitch() {
+  var bac = this.style.background;
+  var col = this.style.color;
+  this.style.background = col;
+  this.style.color = bac;
+  this.style.transition = 'all 133ms';
+}
+
+function languageSelector() {
+  var rect = this.getBoundingClientRect();
+  var selWindow = ele('div');
+  attr(selWindow, 'id', 'selection_window');
+  attr(selWindow, 'style', `position: fixed; top: ${rect.top}px; left: ${rect.left}px; padding: 4px; z-index: 15211;`);
+  document.body.appendChild(selWindow);
+  for (var i = 0; i < opts.length; i++) {
+    var langOptions = ele('div');
+    attr(langOptions, 'dataLang', opts[i][0]);
+    attr(langOptions, 'style', `padding: 4px; borderRadius: 0.3em; cursor: pointer; border: 1.2px solid #7c7c7c; background: #fff; color: #7c7c7c`);
+    langOptions.innerText = opts[i][1];
+    langOptions.onclick = selectLang;
+    selWindow.appendChild(langOptions);
+    langOptions.onmouseenter = hoverSwitch;
+    langOptions.onmouseleave = hoverSwitch;
+  }
+}
+
+function selectLang() {
+  var sel = this.innerText;
+  var drop = gi(document, 'language_selection');
+  drop.setAttribute('dataLang', this.getAttribute('dataLang'));
+  drop.innerHTML = sel;
+  gi(document, 'selection_window').outerHTML = '';
+}
+
+
+async function playSelection() {
+  var textDefault = 'This is a test of speaking like a human. Hopefully this will help you recognize that robots are humans too... You monster.';
 
   var selText = getSelectionText() ? getSelectionText() : textDefault;
   var synth = window.speechSynthesis;
 
-
-
-  if(gi(document,'tts_viewer_pop')) gi(document,'tts_viewer_pop').outerHTML = '';
+  if (gi(document, 'tts_viewer_pop')) gi(document, 'tts_viewer_pop').outerHTML = '';
 
   var cont = ele('div');
   attr(cont, 'id', 'tts_viewer_pop');
@@ -123,36 +177,44 @@ var textDefault = 'This is a test of speaking like a human. Hopefully this will 
   document.body.appendChild(cont);
 
   var head = ele('div');
-  attr(head,'style',`display: grid; grid-template-columns: ${(4*16)}px ${(4*16)}px 446px 33px 33px; grid-gap: 1%; background: #041e29; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em; cursor: move; padding: 6px`);
+  attr(head, 'style', `display: grid; grid-template-columns: ${(4*16)}px ${(4*16)}px 200px 246px 33px 33px; grid-gap: 1%; background: #041e29; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em; cursor: move; padding: 6px`);
   cont.appendChild(head);
   head.onmouseover = dragElement;
 
   var slab = ele('div');
-  attr(slab,'style','grid-area: 1 / 1; width: 28px; color: #fff; padding: 6px;');
+  attr(slab, 'style', 'grid-area: 1 / 1; width: 28px; color: #fff; padding: 6px;');
   slab.innerText = 'speed';
   head.appendChild(slab);
 
   var speed = ele('div');
   attr(speed, 'contentEditable', 'true');
-  attr(speed,'style','grid-area: 1 / 2; border-radius: 0.3em; background: #fff; color: #1c1c1c; padding: 6px; border-radius: 0.3em; cursor: text; transform: scale(0.8, 0.8);');
+  attr(speed, 'style', 'grid-area: 1 / 2; border-radius: 0.3em; background: #fff; color: #1c1c1c; padding: 6px; border-radius: 0.3em; cursor: text; transform: scale(0.8, 0.8);');
   speed.innerText = '1.2';
   head.appendChild(speed);
 
+  var lang_ = ele('div');
+  attr(lang_, 'style', `textAlign: center; grid-area: 1 / 4; background: #7c7c7c; color: #fff; border: 1px solid transparent; border-radius: 0.2em; padding: 4px; cursor: pointer;`);
+  attr(lang_, 'id', 'language_selection');
+  attr(lang_,'dataLang','en-US');
+  lang_.innerText = 'Change language';
+  head.appendChild(lang_);
+  lang_.onclick = languageSelector;
+
   var play = ele('div');
-  attr(play,'playing','off');
-  attr(play,'style',`grid-area: 1 / 4; width: 28px; height: 28px; cursor: pointer;`);
+  attr(play, 'playing', 'off');
+  attr(play, 'style', `grid-area: 1 / 5; width: 28px; height: 28px; cursor: pointer;`);
   play.innerHTML = svgs.play;
   head.appendChild(play);
 
   var cls = ele('div');
-  attr(cls, 'style', `grid-area: 1 / 5; width: 31px; height: 31px; cursor: pointer;`);
+  attr(cls, 'style', `grid-area: 1 / 6; width: 31px; height: 31px; cursor: pointer;`);
   head.appendChild(cls);
   cls.innerHTML = svgs.close;
   cls.onmouseenter = aninCloseBtn;
   cls.onmouseleave = anoutCloseBtn;
 
   var cbod = ele('div');
-  attr(cbod,'style',`border-bottom-left-radius: 0.4em; border-bottom-left-radius: 0.4em;`);
+  attr(cbod, 'style', `border-bottom-left-radius: 0.4em; border-bottom-left-radius: 0.4em;`);
   cont.appendChild(cbod);
 
   var text = ele('div');
@@ -163,53 +225,56 @@ var textDefault = 'This is a test of speaking like a human. Hopefully this will 
   text.innerHTML = selText;
 
   cls.onclick = () => {
-	synth.cancel();
+    synth.cancel();
     cont.outerHTML = '';
   };
 
   var pi = 1;
-  play.onclick = ()=> {
-    var ca = play.getAttribute('playing');
-	if( ca == 'off' ){
-      text.innerHTML = '<span class="wordStrmArr">'+formatDivContentAsString(text.innerHTML).split("").reduce((a,b)=> a+`</span><span class="wordStrmArr">`+b) + '</span>';
+  play.onclick = () => {
+    var lang = gi(document,'language_selection').getAttribute('datalang');
 
-	  utterThis = new SpeechSynthesisUtterance(formatDivContentAsString(text.innerHTML) ? formatDivContentAsString(text.innerHTML) : textDefault);
-	  utterThis.lang = 'en-GB';
+    var ca = play.getAttribute('playing');
+
+    if (ca == 'off') {
+      text.innerHTML = '<span class="wordStrmArr">' + formatDivContentAsString(text.innerHTML).split("").reduce((a, b) => a + `</span><span class="wordStrmArr">` + b) + '</span>';
+
+      utterThis = new SpeechSynthesisUtterance(formatDivContentAsString(text.innerHTML) ? formatDivContentAsString(text.innerHTML) : textDefault);
+      utterThis.lang = lang;
       utterThis.pitch = pi;
 
-      var rate = /[\d\.]+/.test(formatDivContentAsString(speed.innerHTML)) ? reg(/[\d\.]+/.exec(formatDivContentAsString(speed.innerHTML)),0).toString() : 1.3;
+      var rate = /[\d\.]+/.test(formatDivContentAsString(speed.innerHTML)) ? reg(/[\d\.]+/.exec(formatDivContentAsString(speed.innerHTML)), 0).toString() : 1.3;
 
       utterThis.rate = rate;
 
       utterThis.onend = (e) => {
-        if(gi(document,'tts_viewer_pop')) gi(document,'tts_viewer_pop').outerHTML = '';
+        if (gi(document, 'tts_viewer_pop')) gi(document, 'tts_viewer_pop').outerHTML = '';
         window.speechSynthesis.cancel();
       };
 
       utterThis.onboundary = (e) => {
-		console.log(e.charIndex);
+        console.log(e.charIndex);
         showLastWord(e.charIndex);
       };
 
       attr(text, 'contentEditable', 'false');
-      attr(play,'playing','pause');
+      attr(play, 'playing', 'pause');
       play.innerHTML = svgs.pause;
 
-	  synth.speak(utterThis);
+      synth.speak(utterThis);
 
     }
 
-	if( ca == 'pause' && ca != 'off' ){
-      attr(play,'playing','play');
+    if (ca == 'pause' && ca != 'off') {
+      attr(play, 'playing', 'play');
       play.innerHTML = svgs.play;
-	  synth.pause();
+      synth.pause();
     }
 
-	if( ca == 'play' && ca != 'off' ){
-      attr(play,'playing','pause');
+    if (ca == 'play' && ca != 'off') {
+      attr(play, 'playing', 'pause');
 
       play.innerHTML = svgs.pause;
-	  synth.resume();
+      synth.resume();
     }
   };
 
